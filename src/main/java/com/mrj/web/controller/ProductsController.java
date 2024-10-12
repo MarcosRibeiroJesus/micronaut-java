e5e4ec3c787188041e4125861f40ba6a78fc7071
MarcosRibeiroJesus <mrj84@outlook.com>
2024-10-11 23:01:20 -0300

    fix: Query Parameters

--- src/main/java/com/mrj/web/controller/ProductsController.java
+++ src/main/java/com/mrj/web/controller/ProductsController.java
@@ -5,9 +5,11 @@ import com.mrj.web.model.Product;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.PathVariable;
+import io.micronaut.http.annotation.QueryValue;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller("/products")
 public class ProductsController {
@@ -26,6 +28,18 @@ public class ProductsController {
     @Get("{id}")
     public Product getProduct(@PathVariable Integer id) {
         return store.getProducts().get(id);
+    }
+
+    @Get("/filter{?max,offset}")
+    public List<Product> filteredProducts(
+            @QueryValue Optional<Integer> max,
+            @QueryValue Optional<Integer> offset) {
+        return store.getProducts()
+                .values()
+                .stream()
+                .skip(offset.orElse(0))
+                .limit(max.orElse(0))
+                .toList();
     }
 }
 
