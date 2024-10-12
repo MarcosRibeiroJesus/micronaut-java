ae302b4c2ff2da3f839f826d94d623114fbd27e1
MarcosRibeiroJesus <mrj84@outlook.com>
2024-10-11 23:45:12 -0300

    fix: Query Parameters JUnit

--- src/test/java/com/mrj/web/controller/ProductsControllerTest.java
+++ src/test/java/com/mrj/web/controller/ProductsControllerTest.java
@@ -45,6 +45,22 @@ class ProductsControllerTest {
         assertNotNull(response.name());
     }
 
+    @Test
+    void canLimitTheAmountOfProductsToFetchTo5() throws IOException {
+        JsonNode response = client.toBlocking().retrieve("/filter?max=5", JsonNode.class);
+        LOG.debug("Retrieved 5 products: {}", logProducts(response));
+        assertEquals(5, response.size());
+    }
+
+    @Test
+    void canFilterUsing() throws IOException {
+        JsonNode response = client.toBlocking().retrieve("/filter?max=2&offset=6", JsonNode.class);
+        LOG.debug("Retrieved 2 products starting with offset 6: {}", logProducts(response));
+        assertEquals(2, response.size());
+        assertEquals(6, response.get(0).get("id").getIntValue());
+        assertEquals(7, response.get(1).get("id").getIntValue());
+    }
+
     private String logProducts(JsonNode response) throws IOException {
         return jsonMapper.writeValueAsString(response);
     }
